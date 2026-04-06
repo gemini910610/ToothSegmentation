6f127570943711e0c4bacef8f65510d1961f671d
unknown <gemini910610@gmail.com>
2026-04-06 22:55:42 +0800

    add slice only viewer mode

--- keypoint/predict.py
+++ keypoint/predict.py
@@ -3,6 +3,8 @@ import numpy
 
 from keypoint.models import KeypointModel
 from torchvision import transforms
+from scipy import ndimage
+from scripts.tools.widgets import Label
 
 class CEJFinder:
     def __init__(self, model_path):
@@ -19,7 +21,7 @@ class CEJFinder:
         image = self.transform(image)
         return image
 
-    def find(self, tooth_slices):
+    def find(self, segmentation_slices, tooth_slices):
         images = torch.stack([self._preprocess_image(image) for image in tooth_slices])
         images = images.cuda()
 
@@ -27,13 +29,33 @@ class CEJFinder:
             predicts = self.model(images)
 
         points = []
-        for tooth_slice, (left_x, left_y, right_x, right_y) in zip(tooth_slices, predicts):
+        for segmentation_slice, tooth_slice, (left_x, left_y, right_x, right_y) in zip(segmentation_slices, tooth_slices, predicts):
             height, width = tooth_slice.shape
             left_x = (left_x * width).item()
             left_y = (left_y * height).item()
             right_x = (right_x * width).item()
             right_y = (right_y * height).item()
 
-            points.append((left_x, left_y, right_x, right_y))
+            point = self._move_to_surface(segmentation_slice, left_x, left_y, right_x, right_y)
+            points.append(point)
         points = numpy.array(points)
         return points
+
+    def _move_to_surface(self, segmentation_slice, left_x, left_y, right_x, right_y):
+        tooth_area = segmentation_slice == Label.TOOTH
+        structure = ndimage.generate_binary_structure(2, 1)
+        erosion = ndimage.binary_erosion(tooth_area, structure)
+        tooth_surface = tooth_area & ~erosion
+        ys, xs = numpy.where(tooth_surface)
+
+        distances = (xs - left_x) ** 2 + (ys - left_y) ** 2
+        index = numpy.argmin(distances)
+        left_x = xs[index]
+        left_y = ys[index]
+
+        distances = (xs - right_x) ** 2 + (ys - right_y) ** 2
+        index = numpy.argmin(distances)
+        right_x = xs[index]
+        right_y = ys[index]
+
+        return left_x, left_y, right_x, right_y
